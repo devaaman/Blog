00913fd52d98f9ae2614f57a9d38ad9feda44653
devaaman33 <devaaman33@gmail.com>
2024-02-11 21:36:52 +0530

    blog App

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import axios from "axios"
+import Footer from "../components/Footer"
+import HomePosts from "../components/HomePosts"
+import Navbar from "../components/Navbar"
+import { IF, URL } from "../url"
+import { useContext, useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
+import Loader from '../components/Loader'
+import { UserContext } from "../context/UserContext"
+ 
+
+const Home = () => {
+  
+  const {search}=useLocation()
+  // console.log(search)
+  const [posts,setPosts]=useState([])
+  const [noResults,setNoResults]=useState(false)
+  const [loader,setLoader]=useState(false)
+  const {user}=useContext(UserContext)
+  // console.log(user)
+
+  const fetchPosts=async()=>{
+    setLoader(true)
+    try{
+      const res=await axios.get(URL+"/api/posts/"+search)
+      // console.log(res.data)
+      setPosts(res.data)
+      if(res.data.length===0){
+        setNoResults(true)
+      }
+      else{
+        // otherwise it dont work
+        setNoResults(false)
+      }
+      setLoader(false)
+      
+    }
+    catch(err){
+      console.log(err)
+      setLoader(true)
+    }
+  }
+// use effect is rendered in every search
+
+
+
+
+  useEffect(()=>{
+    fetchPosts()
+
+  },[search])
+
+
+
+  return (
+    
+    <>
+    <Navbar/>
+<div className="px-8 md:px-[200px] min-h-[80vh]">
+{/* if loading show loader or if not load ing and hav some results use map funcction to show it other wiswe show no Post Available */}
+        {loader?<div className="h-[40vh] flex justify-center items-center"><Loader/></div>:!noResults?
+        posts.map((post)=>(
+          <>
+          {/* if user go to post detail page  else go to login paage */}
+          <Link to={user?`/posts/post/${post._id}`:"/login"}>
+          <HomePosts key={post._id} post={post}/>
+          </Link>
+          </>
+          
+        )):<h3 className="text-center font-bold mt-16">No posts available</h3>}
+    </div>
+    <Footer/>
+    </>
+    
+  )
+}
+
+export default Home
